c0f4a4b1111b9b8432fbb27462b75d73d38db47c
vinicius <vinicius.henrique.92@hotmail.com>
2022-10-04 09:45:46 -0300

    Upload das capas dos cursos

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -1,17 +1,18 @@
 import { ResourceWithOptions } from "adminjs";
 import { Category, Course, Episode } from "../../models";
 import { categoryResourceOptions } from "./category";
-import { courseResourceOptions } from "./course";
+import { courseResourceFeatures, courseResourceOptions } from "./course";
 import { episodeResourceFeatures, episodeResourceOptions } from "./episode";
 
 export const adminJsResources: ResourceWithOptions[] = [
     {
-        resource: Course,
-        options: courseResourceOptions
-    },
-    {
         resource: Category,
         options: categoryResourceOptions
+    },
+    {
+        resource: Course,
+        options: courseResourceOptions,
+        features: courseResourceFeatures
     },
     {
         resource: Episode,
