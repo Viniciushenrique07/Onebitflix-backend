8426936b191368c34309474a79c47d2830c032df
vinicius <vinicius.henrique.92@hotmail.com>
2022-10-07 10:06:14 -0300

    Obtendo informações do curso

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { categoriesController } from "./controllers/categoriesController";
+import { coursesControler } from "./controllers/coursesController";
 
 const router = express.Router();
 
 router.get("/categories",  categoriesController.index)
 router.get("/categories/:id", categoriesController.show)
+router.get("/courses/:id", coursesControler.show)
 
 export { router }
