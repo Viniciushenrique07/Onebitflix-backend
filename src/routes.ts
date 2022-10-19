fab3f61fa263018a6cb9e7f670fcf7cbf3f37844
vinicius <vinicius.henrique.92@hotmail.com>
2022-10-19 11:42:12 -0300

    Middleware de autorização

--- src/routes.ts
+++ src/routes.ts
@@ -3,19 +3,20 @@ import { authController } from "./controllers/authController";
 import { categoriesController } from "./controllers/categoriesController";
 import { coursesControler } from "./controllers/coursesController";
 import { episodesController } from "./controllers/episodesController";
+import { ensureAuth } from "./middlewares/auth";
 
 const router = express.Router();
 
 router.post("/auth/register", authController.register)
 router.post("/auth/login", authController.login)
 
-router.get("/categories",  categoriesController.index)
-router.get("/categories/:id", categoriesController.show)
+router.get("/categories", ensureAuth, categoriesController.index)
+router.get("/categories/:id", ensureAuth, categoriesController.show)
 
-router.get("/courses/featured", coursesControler.featured)
+router.get("/courses/featured", ensureAuth, coursesControler.featured)
 router.get("/courses/newest", coursesControler.newest)
-router.get("/courses/search", coursesControler.search)
-router.get("/courses/:id", coursesControler.show)
+router.get("/courses/search", ensureAuth, coursesControler.search)
+router.get("/courses/:id", ensureAuth, coursesControler.show)
 
 router.get("/episodes/stream", episodesController.stream)
 
